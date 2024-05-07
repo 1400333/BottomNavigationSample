ede959585283f94ab6e8efb40831f82dcd24a57b
1400333 <wanrulin@softmobile.com.tw>
2024-05-07 16:14:16 +0800

    SubScreen預覽效果
    SubScreen預覽效果

--- app/src/main/java/com/example/bottomnavigationsample/ui/screen/SubScreen.kt
+++ app/src/main/java/com/example/bottomnavigationsample/ui/screen/SubScreen.kt
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.bottomnavigationsample.ui.custom.NavigationButton
 import com.example.bottomnavigationsample.ui.theme.BottomNavigationSampleTheme
 
@@ -36,4 +38,12 @@ fun MyScreenTitle(title: String, modifier: Modifier = Modifier) {
              .fillMaxWidth()
              .padding(top = 16.dp, bottom = 16.dp),
          textAlign = TextAlign.Center)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SubScreenPreview() {
+    val navController = rememberNavController()
+
+    SubScreen(navController,"預覽")
 }
